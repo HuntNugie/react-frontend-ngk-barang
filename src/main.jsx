b632433c88d1bbc10306f4c941e69951c732f889
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-14 11:05:31 +0700

    context:refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
-import {AuthProvider} from "./context/Auth.context.jsx";
+import {AuthProvider} from "./provider/Auth.provider.jsx";
 import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
---
+++ src/provider/Auth.provider.jsx
@@ -0,0 +1,28 @@
+import { AuthContext } from "../context/Auth.context";
+import { useState,useEffect } from "react";
+import axios from "axios";
+export const AuthProvider = ({children}) => {
+    const [isAuth, setIsAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [User, setUser] = useState(null);
+    const api = import.meta.env.VITE_BACKEND_API;
+    useEffect(() => {
+        const request = async () => {
+            try {
+                const res = await axios.get(`${api}/api/auth/me`, {withCredentials: true});
+                const data = res.data;
+                setUser(data);
+                setIsAuth(true);
+            } catch (error) {
+                setUser(null);
+                setIsAuth(false);
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        request();
+    }, []);
+
+    return <AuthContext.Provider value={{User, loading, isAuth}}>{children}</AuthContext.Provider>;
+};
